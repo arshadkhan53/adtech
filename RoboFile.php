67c249c4eb899c6623fbaf5d73266c48cfee91aa
Mohammed Arshad <arshadkhan2631@gmail.com>
2022-05-14 12:42:57 +0530

    Added custom module path.

--- RoboFile.php
+++ RoboFile.php
@@ -8,25 +8,35 @@
  */
 class RoboFile extends \Robo\Tasks
 {
-
-    public function __construct()
-    {
+    /**
+     * Implements constructor
+     */
+    public function __construct() {
         $this->stopOnFail();
     }
 
+    /**
+     * Run coding standard validation.
+     *
+     * @return void
+     */
     public function jobRunCodingStandardCheck() {
         return $this->taskExecStack()
         ->stopOnFail()
         ->exec('vendor/bin/phpcs --config-set installed_path vendor/drupal/coder/coder_sniffer')
-        ->exec('vendor/bin/phpcs --standard=Drupal web/modules')
-        ->exec('vendor/bin/phpcs --standard=DrupalPractice web/modules')
+        ->exec('vendor/bin/phpcs --standard=Drupal web/modules/custom')
+        ->exec('vendor/bin/phpcs --standard=DrupalPractice web/modules/custom')
         ->run();
     }
-
+    /**
+     * Run test.
+     *
+     * @return void
+     */
     public function jobRunUnitTest() {
         return $this->taskExecStack()
         ->stopOnFail()
-        ->exec('ddev exec -s web vendor/bin/phpunit -c web/core --verbose web/modules')
+        ->exec('ddev exec -s web vendor/bin/phpunit -c web/core --verbose web/modules/custom')
         ->run();
     }
 }
