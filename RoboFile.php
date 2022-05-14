768a40b1344ae55c28d1a9ea817f1dd76c1ba52d
Mohammed Arshad <arshadkhan2631@gmail.com>
2022-05-14 13:26:31 +0530

    Added pass scenarios.

--- RoboFile.php
+++ RoboFile.php
@@ -24,8 +24,11 @@ class RoboFile extends \Robo\Tasks
         return $this->taskExecStack()
         ->stopOnFail()
         ->exec('vendor/bin/phpcs --config-set installed_path vendor/drupal/coder/coder_sniffer')
-        ->exec('vendor/bin/phpcs --standard=Drupal web/modules/custom')
-        ->exec('vendor/bin/phpcs --standard=DrupalPractice web/modules/custom')
+        // ->exec('vendor/bin/phpcs --standard=Drupal web/modules/custom')
+        // ->exec('vendor/bin/phpcs --standard=DrupalPractice web/modules/custom')
+        // This is to just test the script, remove below two lines and uncomment above for custom modules.
+        ->exec('vendor/bin/phpcs --standard=Drupal web/core/modules/action/tests/src/Functional/ActionListTest.php')
+        ->exec('vendor/bin/phpcs --standard=DrupalPractice web/core/modules/action/tests/src/Functional/ActionListTest.php')
         ->run();
     }
 
@@ -37,7 +40,9 @@ class RoboFile extends \Robo\Tasks
     public function jobRunUnitTest() {
         return $this->taskExecStack()
         ->stopOnFail()
-        ->exec('ddev exec -s web vendor/bin/phpunit -c web/core --verbose web/modules/custom')
+        // ->exec('ddev exec -s web vendor/bin/phpunit -c web/core --verbose web/modules/custom')
+        // This is to just test the script, remove below line and uncomment above for custom modules.
+        ->exec('ddev exec -s web vendor/bin/phpunit -c web/core --verbose web/core/modules/action/tests/src/Unit/Menu/ActionLocalTasksTest.php')
         ->run();
     }
 
@@ -51,6 +56,23 @@ class RoboFile extends \Robo\Tasks
         ->stopOnFail()
         ->exec('vendor/bin/robo job:run-coding-standard-check')
         ->exec('vendor/bin/robo job:run-unit-test')
+        ->run();
+    }
+
+    /**
+     * Run test.
+     *
+     * @return void
+     */
+    public function jobRunDeploy() {
+        return $this->taskExecStack()
+        ->stopOnFail()
+        // ->exec('git remote add upstream [YOUR_SERVER_REMOTE_LIKE_PANTHEON_OR_AQUIA]')
+        // ->exec('git push upstream')
+        ->exec('ddev auth ssh')
+        // ->exec('ddev drush @site_alias cim -y')
+        // ->exec('ddev drush @site_alias updb -y')
+        // ->exec('ddev drush @site_alias cr')
         ->run();
     }
 }
